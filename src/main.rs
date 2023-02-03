095a85407ae92e982d212ddf97282926d41f6830
Alex Coleman <alexjcoleman@gmail.com>
2023-02-03 15:20:31 +0000

    make a basic echo clone with lexopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+struct Args {
+    thing: String,
+}
+
+fn parse_args() -> Result<Args, lexopt::Error> {
+    use lexopt::prelude::*;
+
+    let mut thing = None;
+    let mut parser = lexopt::Parser::from_env();
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Value(val) if thing.is_none() => {
+                thing = Some(val.string()?);
+            }
+            Long("help") => {
+                println!("Usage: shouty [-n|--number=NUM] [--shout] THING");
+                std::process::exit(0);
+            }
+            _ => return Err(arg.unexpected()),
+        }
+    }
+
+    Ok(Args {
+        thing: thing.ok_or("missing argument THING")?,
+    })
+}
+
+fn main() -> Result<(), lexopt::Error> {
+    let args = parse_args()?;
+    println!("{}", args.thing);
+    Ok(())
 }
