a212743f9176e6159ae3bef914818aa96556b508
Alex Coleman <alexjcoleman@gmail.com>
2023-02-03 15:25:19 +0000

    adding shout flag

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 struct Args {
     thing: String,
+    shout: bool
 }
 
 fn parse_args() -> Result<Args, lexopt::Error> {
     use lexopt::prelude::*;
 
     let mut thing = None;
+    let mut shout = false;
     let mut parser = lexopt::Parser::from_env();
     while let Some(arg) = parser.next()? {
         match arg {
+            Short('s') | Long("--shout") => {
+                shout = true;
+            }
             Value(val) if thing.is_none() => {
                 thing = Some(val.string()?);
             }
@@ -22,11 +27,18 @@ fn parse_args() -> Result<Args, lexopt::Error> {
 
     Ok(Args {
         thing: thing.ok_or("missing argument THING")?,
+        shout
     })
 }
 
 fn main() -> Result<(), lexopt::Error> {
     let args = parse_args()?;
-    println!("{}", args.thing);
+    let mut message = args.thing;
+
+    if args.shout {
+        message = message.to_uppercase();
+    }
+
+    println!("{}", message);
     Ok(())
 }
